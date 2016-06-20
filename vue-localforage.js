82e1c5bcb7dc3ce724d272751f632cbba1bba720
Andrew Kalek <andrew.kalek@anlek.com>
2016-06-19 20:06:35 -0700

    Added return value to allow working with promises Also removed unused options variable

--- vue-localforage.js
+++ vue-localforage.js
@@ -7,49 +7,47 @@
 
 var localForage = require('localforage')
 
-function VueLocalForage(Vue) {
-  var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1]
-  
+function VueLocalForage (Vue) {
+
   Vue.prototype.$getItem = function (key, callback) {
-    localForage.getItem(key, callback)
+    return localForage.getItem(key, callback)
   }
-  
+
   Vue.prototype.$setItem = function (key, value, callback) {
-    localForage.setItem(key, value, callback)
+    return localForage.setItem(key, value, callback)
   }
-  
+
   Vue.prototype.$removeItem = function (key, callback) {
-    localForage.removeItem(key, callback)
+    return localForage.removeItem(key, callback)
   }
-  
+
   Vue.prototype.$clearStorage = function () {
     localForage.clear()
   }
-  
-  Vue.prototype.$lengthOfStorage = function () {
-    localForage.length()
+
+  Vue.prototype.$lengthOfStorage = function (callback) {
+    return localForage.length(callback)
   }
-  
+
   Vue.prototype.$keyInStorage = function (keyIndex, callback) {
-    localForage.key(keyIndex, callback)
+    return localForage.key(keyIndex, callback)
   }
-  
+
   Vue.prototype.$keysInStorage = function (callback) {
-    localForage.keys(callback)
+    return localForage.keys(callback)
   }
-  
+
   Vue.prototype.$iterateStorage = function (iteratorCallback, callback) {
-    localForage.iterate(iteratorCallback, callback)
+    return localForage.iterate(iteratorCallback, callback)
   }
-  
+
   Vue.prototype.$setStorageDriver = function (driverName) {
     localForage.setDriver(driverName)
   }
-  
+
   Vue.prototype.$storageConfig = function (options) {
     localForage.config(options)
   }
-  
 }
 
 VueLocalForage.version = '0.2.0'
